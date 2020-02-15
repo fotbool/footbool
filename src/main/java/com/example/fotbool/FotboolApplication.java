2f624093db34ed69db3c02d66113fbeff627ff70
GorHovsepyan <60837181+GorHovsepyan@users.noreply.github.com>
2020-02-15 20:37:53 +0400

    new

---
+++ src/main/java/com/example/fotbool/FotboolApplication.java
@@ -0,0 +1,13 @@
+package com.example.fotbool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FotboolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FotboolApplication.class, args);
+	}
+
+}
